a991fd85fe5ab8bb46adee17cd7ded1e98bf7aa3
Devin Mooers <dkmooers@gmail.com>
2022-08-19 11:33:24 -0400

    Add slider component for future time-sliding replays

---
+++ src/components/Slider.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  export let min = 0
+  export let max = 100
+  export let value = 50
+</script>
+
+<label for="default-range" class="block text-sm font-medium">Year</label>
+<input id="default-range" type="range" {min} {max} bind:value={value} class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700">
